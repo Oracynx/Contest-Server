401dd8dae45c04c18b1e9050f97741f0bfcb3b86
Oracynx <oracynx@gmail.com>
2025-11-25 13:06:08 +0800

    添加返回

--- static/ts/index.ts
+++ static/ts/index.ts
@@ -1,11 +1,3 @@
-import { init, initClientWS, showSuccess } from './utils/base';
+import { init } from './utils/base';
 
 init('Home', false);
-
-const testBtn = document.getElementById('test-btn') as HTMLButtonElement;
-
-testBtn.onclick = () =>
-{
-    console.log('Button clicked!');
-    showSuccess('Button was clicked successfully!');
-}
