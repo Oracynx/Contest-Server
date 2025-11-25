5629b431e304f07b230c560b4c32901127731776
Oracynx <oracynx@gmail.com>
2025-11-25 08:33:14 +0800

    添加主入口

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import Elysia from 'elysia';
+import { clientApp } from './client';
+import { serverApp } from './server';
+import { adminApp } from './admin';
 
-const app = new Elysia();
+const app = new Elysia()
+    .use(clientApp)
+    .use(serverApp)
+    .use(adminApp)
 
 app.listen(8080);
+
+for (const route of app.routes)
+{
+    console.log(`${route.method} ${route.path}`)
+}
